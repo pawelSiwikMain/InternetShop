54a8aac1464e541ce8fba033ebfb7bac308933d5
Mateusz Skubiszak <mateusz.skubiszak1@wp.pl>
2023-12-14 15:39:36 +0100

    Adds ProductDetail and Cart view

---
+++ Frontend/shopFront/src/views/CartView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+import Cart from "@/components/Cart.vue";
+</script>
+
+<template>
+
+  <NavBar/>
+  <Cart/>
+
+</template>
+
+<style scoped>
+
+</style>
