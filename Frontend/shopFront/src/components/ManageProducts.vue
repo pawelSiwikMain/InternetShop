a6a09de4a0ed6ba4c31a87e7e31928aac0ae6a19
Mateusz Skubiszak <mateusz.skubiszak1@wp.pl>
2024-01-07 22:51:16 +0100

    Adds Manager

--- Frontend/shopFront/src/components/ManageProducts.vue
+++ Frontend/shopFront/src/components/ManageProducts.vue
@@ -5,11 +5,12 @@
     <h5>Products in Shop</h5>
     <hr>
 
-    <div class="Add">
+    <div v-if="!isAddProductFormOpen && !isEditProductFormOpen" class="Add">
       <button @click="openAddProductForm" class="btn btn-primary">Add Product</button>
     </div>
 
     <div v-if="isAddProductFormOpen" class="AddProductForm">
+      <h5>Add new product</h5>
       <input required v-model="name" class="form-control mb-2" placeholder="Name">
       <input required v-model="description" class="form-control mb-2" placeholder="Description">
       <input required v-model="category" class="form-control mb-2" placeholder="Category">
@@ -21,7 +22,7 @@
     </div>
 
     <hr>
-    <table class="table table-bordered table-hover text-center">
+    <table v-if="!isAddProductFormOpen && !isEditProductFormOpen" class="table table-bordered table-hover text-center">
       <thead class="thead-dark">
       <tr>
         <th>ID</th>
@@ -46,19 +47,32 @@
         <td>
           <button @click="editProduct(product)" class="btn btn-warning mr-2">Edit</button>
           <button @click="deleteProduct(product.id)" class="btn btn-danger">Delete</button>
-
         </td>
       </tr>
       </tbody>
     </table>
+
+    <div v-if="isEditProductFormOpen" class="EditProductForm">
+      <h5>Edit product</h5>
+      <input required v-model="editedProduct.name" class="form-control mb-2" placeholder="Name">
+      <input required v-model="editedProduct.description" class="form-control mb-2" placeholder="Description">
+      <input required v-model="editedProduct.category" class="form-control mb-2" placeholder="Category">
+      <input required v-model="editedProduct.price" class="form-control mb-2" placeholder="Price">
+      <input required v-model="editedProduct.quqntityInStorage" class="form-control mb-2" placeholder="Quantity">
+      <input required v-model="editedProduct.urlToPicture" class="form-control mb-2" placeholder="urlToPicture">
+      <button @click="updateProduct" class="btn btn-success">Save</button>
+      <button @click="closeEditProductForm" class="btn btn-secondary">Cancel</button>
+    </div>
+
   </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted, reactive} from 'vue';
 
 const isAddProductFormOpen = ref(false);
+const isEditProductFormOpen = ref(false);
 
 const products = ref([]);
 const name = ref('');
@@ -72,6 +86,12 @@ const openAddProductForm = () => {
 };
 
 const closeAddProductForm = () => {
+  name.value = '';
+  description.value = '';
+  category.value = '';
+  price.value = '';
+  quqntityInStorage.value = '';
+  urlToPicture.value = '';
   isAddProductFormOpen.value = false;
 };
 const fetchProducts = async () => {
@@ -111,12 +131,6 @@ const addProduct = async () => {
 
     if (response.ok) {
       fetchProducts();
-      name.value = '';
-      description.value = '';
-      category.value = '';
-      price.value = '';
-      quqntityInStorage.value = '';
-      urlToPicture.value = '';
       closeAddProductForm();
     } else {
       throw new Error('Failed to add product');
@@ -126,9 +140,66 @@ const addProduct = async () => {
   }
 };
 
+const editedProduct = reactive({
+  id: null,
+  name: '',
+  description: '',
+  category: '',
+  price: 0,
+  quqntityInStorage: 0,
+  urlToPicture: '',
+});
 
 const editProduct = (product) => {
-  console.log('Editing product:', product);
+  editedProduct.id = product.id;
+  editedProduct.name = product.name;
+  editedProduct.description = product.description;
+  editedProduct.category = product.category;
+  editedProduct.price = product.price;
+  editedProduct.quqntityInStorage = product.quqntityInStorage;
+  editedProduct.urlToPicture = product.urlToPicture;
+
+  isEditProductFormOpen.value = true;
+};
+
+const closeEditProductForm = () => {
+  isEditProductFormOpen.value = false;
+  editedProduct.id = null;
+  editedProduct.name = '';
+  editedProduct.description = '';
+  editedProduct.category = '';
+  editedProduct.price = 0;
+  editedProduct.quqntityInStorage = 0;
+  editedProduct.urlToPicture = '';
+};
+
+const updateProduct = async () => {
+  try {
+    const requestData = {
+      id: editedProduct.id,
+      name: editedProduct.name,
+      description: editedProduct.description,
+      category: editedProduct.category,
+      price: editedProduct.price,
+      quqntityInStorage: editedProduct.quqntityInStorage,
+      urlToPicture: editedProduct.urlToPicture,
+    };
+
+    const response = await fetch("https://localhost:44396/api/Products", {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(requestData),
+    });
+
+    if (response.ok) {
+      fetchProducts();
+      closeEditProductForm();
+    } else {
+      throw new Error('Failed to update product');
+    }
+  } catch (error) {
+    console.error('Error updating product:', error);
+  }
 };
 
 const deleteProduct = (productId) => {
@@ -144,6 +215,8 @@ const deleteProduct = (productId) => {
       })
       .catch(error => console.error('Error:', error));
 };
+
+
 </script>
 
 <style scoped>
